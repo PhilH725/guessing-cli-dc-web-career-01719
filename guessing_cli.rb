f5ae0b0e1d1c4fb9fd2681d2e7c43d0442a243d1
Phil Hartley <philh725@gmail.com>
2018-12-31 03:55:41 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -5,6 +5,7 @@ def run_guessing_game
     puts "Guess a number between 1 and 6."
     guess = gets.chomp
     if guess == "exit"
+      puts "Goodbye!"
       break
     end
     if guess.to_i == answer_num
